cfc903ba20d3be2d0225d2482db9517dac1a853d
a815954710 <815954710@qq.com>
2026-05-16 14:21:49 +0800

    新增一键打包功能
    1

---
+++ 一键打包.bat
@@ -0,0 +1,9 @@
+pyinstaller ^
+  --onefile ^
+  --name daledou ^
+  --paths src ^
+  --hidden-import src.daledou.tasks.common ^
+  --hidden-import src.daledou.tasks.one ^
+  --hidden-import src.daledou.tasks.two ^
+  --hidden-import src.daledou.tasks.other ^
+  main.py
